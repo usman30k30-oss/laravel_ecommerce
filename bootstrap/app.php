b7e4688a04eac41e657227d2a6fd2bb8959d6524
HP <.com>
2026-03-31 09:31:59 +0500

    Fix proxy headers

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,10 +12,14 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
 
+
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->trustProxies(
             at: '*',
-            headers: Request::HEADER_X_FORWARDED_ALL
+            headers: Request::HEADER_X_FORWARDED_FOR
+                | Request::HEADER_X_FORWARDED_HOST
+                | Request::HEADER_X_FORWARDED_PORT
+                | Request::HEADER_X_FORWARDED_PROTO
         );
     })
     
